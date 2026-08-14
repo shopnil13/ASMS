9a2148835e504a9bad7b126a3f8569b58be36f61
Rifat Ahmad Shopnil <rifat.shopnil13@gmail.com>
2026-08-14 12:01:47 +0600

    Admin seeder

--- backend/src/Assignment.Api/Program.cs
+++ backend/src/Assignment.Api/Program.cs
@@ -116,6 +116,17 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider
+        .GetRequiredService<ApplicationDbContext>();
+
+    await AdminSeeder.SeedAsync(
+        dbContext,
+        app.Configuration);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
